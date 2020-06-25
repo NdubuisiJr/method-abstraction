2712b1e8a2607e253609edc160b3fc347db80aad
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-06-25 17:23:07 +0100

    Implemented Matrix multiplication for JaJa

--- MethodAbstraction/Program.cs
+++ MethodAbstraction/Program.cs
@@ -3,43 +3,65 @@
 namespace MethodAbstraction {
     class Program {
         static void Main(string[] args) {
-            var parameter = new Parameter(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-            while (parameter.ParamOne < parameter.ParamTen) {
-                if (parameter.ParamOne==1) {
-                    Console.WriteLine("I'm in first if");
-                    IncreaseAllParams(parameter);
-                }
-                else if (parameter.ParamTwo==3) {
-                    Console.WriteLine("I'm in second if");
-                    IncreaseAllParams(parameter);
-                }
-                else if (parameter.ParamFour == 6) {
-                    Console.WriteLine("I'm in three if");
-                    IncreaseAllParams(parameter);
-                }
-                else {
-                    Console.WriteLine($"{parameter.ToString()}, because we are still working with the same object");
-                    IncreaseAllParams(parameter);
+            double[,] A = new double[4, 4];
+            double[,] B = new double[4, 2];
+
+            for (int i = 0; i < A.GetLength(0); i++) {
+                for (int j = 0; j < A.GetLength(1); j++) {
+                    A[i, j] = i + j;
                 }
             }
-            //The next hascode is different because it created a new object
-            Console.WriteLine($"{new Parameter(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).ToString()}, because we are working on a different object now");
 
-            Console.WriteLine("We don finish ooooo!");
+            for (int i = 0; i < B.GetLength(0); i++) {
+                for (int j = 0; j < B.GetLength(1); j++) {
+                    B[i, j] = i + 1 + j + 2;
+                }
+            }
+
+            DisplayArray(A, "A");
+            DisplayArray(B, "B");
+
+            var C = MatrixMultiplication(A, B);
+
+            DisplayArray(C, "C");
+
             Console.ReadLine();
         }
 
-        static void IncreaseAllParams(Parameter parameter) {
-            parameter.ParamOne++;
-            parameter.ParamTwo++;
-            parameter.ParamThree++;
-            parameter.ParamFour++;
-            parameter.ParamFive++;
-            parameter.ParamSix++;
-            parameter.ParamSeven++;
-            parameter.ParamEigth++;
-            parameter.ParamNine++;
-            Console.WriteLine("Performing some magical logic!\n");
+        private static void DisplayArray(double[,] array, string name) {
+            if (array == null)
+                return;
+
+            Console.WriteLine($"\n{name}");
+            Console.WriteLine("---------------------");
+
+            for (int i = 0; i < array.GetLength(0); i++) {
+                for (int j = 0; j < array.GetLength(1); j++) {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static double[,] MatrixMultiplication(double[,] A, double[,] B) {
+            int n1 = A.GetLength(1);
+            int n2 = B.GetLength(0);
+            if (n1 != n2) {
+                Console.WriteLine("The column count of the first array must equal the row count of the second array");
+                return null;
+            }
+
+            double[,] C = new double[A.GetLength(0), B.GetLength(1)];
+
+            for (int i = 0; i < A.GetLength(0); i++) {
+                for (int j = 0; j < B.GetLength(1); j++) {
+                    for (int z = 0; z < n1; z++) {
+                        C[i, j] += A[i, z] * B[z, j];
+                    }
+                }
+            }
+
+            return C;
         }
     }
 }
